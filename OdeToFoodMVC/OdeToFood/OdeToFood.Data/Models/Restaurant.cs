deefe86180b9ea22d01a8b6456b328bc753f96c1
Edward E Greenwood <edwardgreenwood38@gmail.com>
2021-04-11 16:20:49 -0400

    partial entity config. ch 6

--- OdeToFoodMVC/OdeToFood/OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFoodMVC/OdeToFood/OdeToFood.Data/Models/Restaurant.cs
@@ -13,6 +13,7 @@ namespace OdeToFood.Data.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
 
         [Display(Name="Type of food")]
---
+++ OdeToFoodMVC/OdeToFood/OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,43 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+        
+        void IRestaurantData.Add(Restaurant restaurant)
+        {
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+        }
+
+        Restaurant IRestaurantData.Get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        IEnumerable<Restaurant> IRestaurantData.GetAll()
+        {
+            return db.Restaurants.OrderBy(r => r.Name);
+        }
+
+        void IRestaurantData.Update(Restaurant restaurant)
+        {
+            var entry = db.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
