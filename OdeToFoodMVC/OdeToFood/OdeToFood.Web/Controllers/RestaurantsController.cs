d0680f8c412864d69666616dde7b831ba81c3cfb
Edward E Greenwood <edwardgreenwood38@gmail.com>
2021-04-11 14:43:32 -0400

    end ch 5

--- OdeToFoodMVC/OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFoodMVC/OdeToFood/OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -1,4 +1,5 @@
-﻿using OdeToFood.Data.Services;
+﻿using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace OdeToFood.Web.Controllers
             this.db = db;
         }
         
+        [HttpGet]
         // GET: Restaurant
         public ActionResult Index()
         {
@@ -23,6 +25,7 @@ namespace OdeToFood.Web.Controllers
             return View(model);
         }
 
+        [HttpGet]
         public ActionResult Details(int id)
         {
             var model = db.Get(id);
@@ -32,6 +35,56 @@ namespace OdeToFood.Web.Controllers
                 // ### can redirect to an action -  RedirectToAction("Index");
             }
             return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            //if (string.IsNullOrEmpty(restaurant.Name))
+            //{
+            //    ModelState.AddModelError(nameof(restaurant.Name), "the name is required");
+            //}
+            
+            if (ModelState.IsValid)
+            {
+                db.Add(restaurant);
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = db.Get(id);
+
+            if (model == null)
+            {
+                return View("NotFound");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Update(restaurant);
+
+                return RedirectToAction("Details", new { id = restaurant.Id });
+            }
+            return View(restaurant);
         }
     }
 }
